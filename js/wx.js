35c933e3fabdc03d058b72c09f3cedfb3f7318b9
flycdn <45551550+flycdn@users.noreply.github.com>
2020-08-29 13:07:23 +0800

    Update wx.js

--- js/wx.js
+++ js/wx.js
@@ -1,4 +1,4 @@
-var wxArr = ['s18346785792', 'LOVE3286917006', 'YXF888820', 'lv5236117981', 'Yc040625', 'tmn13517156551', 'mwvon22', 'sdn7522'];
+var wxArr =['zcb185q','LOVE3286917006','YXF888820','lv5236117981','Yc040625','tmn13517156551','mwvon22','sdn7522','hh2575868977','jyfLOVExzz'];
 var wx_index = Math.floor((Math.random() * wxArr.length));
 var stxlwx = wxArr[wx_index];
 $(".wx_html").html(stxlwx);
